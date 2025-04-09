d0f233decc0187069f2c463894e15197976c27e1
san san <pts.20.sn@gmail.com>
2025-04-09 08:51:57 +0700

    - Finished: học xong bài 7 và bài 8 - tìm hiểu về Response và Request trong HttpContext - các thuộc tính chứa trong chúng nó - thử debug trên trình duyệt

---
+++ L6_L8_Http/HttpContext_HttpRequest_HttpRespone/HttpContext_HttpRequest_HttpRespone/Controllers/ResponseDemoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContext_HttpRequest_HttpRespone.Controllers;
+
+public class ResponseDemoController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Test()
+    {
+        Response.Headers.Append("X-MyHeader", "MyValue");
+        FileInfo file = new FileInfo("wwwroot/images/aspnet.png");
+        Response.ContentType = "text/html";
+        Response.ContentLength = file.Length;
+        using (var fileStream = file.OpenRead())
+        {
+            await fileStream.CopyToAsync(Response.Body);
+        }
+        return Ok();
+    }
+}
